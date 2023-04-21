1db1e6e57a3dcd711fe60050a30c359c407ff64a
omid ghotbi (TAO) <omidt.gh@gmail.com>
2023-04-22 00:24:52 +0200

    add auth, better refresh rate, better file transfer, add torrent, add env and more
    add auth, better refresh rate, better file transfer, add torrent, add env and more

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Require necessary modules base on Nodejs 11
+// require dotenv library to use environmental variables
+require('dotenv').config();
 const express = require('express');
+const basicAuth = require('express-basic-auth');
+const WebTorrent = require('webtorrent');
 const request = require('request');
-const disk = require('diskusage');
-//const { exec } = require('child_process');
+//const disk = require('diskusage');
+const { exec } = require('child_process');
 const WebSocket = require('ws');
 const https = require('https');
 const path = require('path');
@@ -12,80 +16,211 @@ const fs = require('fs');
 // Create a new Express app
 const app = express();
 
-// Add Basic Authentication middleware to the app
-app.use(basicAuth({
-  users: { 'userName1': 'password1', // Define user userName1 with password password1
-            'userName2': 'password2' }, // Define user userName2 with password password2
-  challenge: true // Enable authentication challenge
-}));
+// Retrieve authentication information from environmental variables
+const UsersLIST = JSON.parse(process.env.userList);
+const PassLIST = JSON.parse(process.env.passList);
 
-// Serve static files from the /css directory
+const usersObj = UsersLIST.reduce((obj, user, index) => {
+  obj[user] = PassLIST[index];
+  return obj;
+}, {});
+
+// Add Basic Authentication middleware to the app
+const auth = basicAuth({
+  users: usersObj,
+  challenge: process.env.Chalenging_Mthod,
+  realm: 'Transload.JS'
+});
+
+// Serve static files from the /css and /images directories
 app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 // Define the directory where downloads will be saved
-const downloadDirectory = '/downloads/';
+const downloadDirectory = process.env.DownloadDirectory;
 
 // Create an HTTPS server with SSL certificate and key you can create SSL certificate using OpenSSL or anyother tools
+// A sample certificate is avilable in /ssl directory change it to your own
 const server = https.createServer({
-  key: fs.readFileSync('ssl/server.key'),
-  cert: fs.readFileSync('ssl/server.cert')
+  key: fs.readFileSync(process.env.SSl_Key),
+  cert: fs.readFileSync(process.env.SSL_Cert)
 }, app);
 
 // Start the server and log a message when it's ready
-server.listen(53014, () => {
-  console.log('Server listening on port 53014');
+server.listen(process.env.PORT, () => {
+  console.log('Server listening on port ' + process.env.PORT);
 });
 
 // Create a new WebSocket server using the HTTPS server
 const wss = new WebSocket.Server({ server });
 
 // Define routes for the app
-app.get('/', (req, res) => {
+app.get('/', auth, (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/contact', auth, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
 
-// Define routes for the actual download process
+app.get('/main.js', function(req, res) {
+  res.setHeader('Content-Type', 'text/javascript');
+  res.sendFile(__dirname + '/public/main.js');
+});
+
+// Download route and function 
 app.get('/download', (req, res) => {
   const urls = req.query.urls.split(',');
   const downloads = urls.map((url, index) => {
-    const localFilename = path.basename(url);
+    let localFilename = path.basename(url);
+    localFilename = localFilename.replace(/(\.[^.]+\?)[\d\W]+$/, '$1').replace(/\?$/, ''); // Remove Symbols 
     const downloadLocation = path.join(downloadDirectory, localFilename);
     let receivedBytes = 0;
     let totalBytes = 0;
+	let lastUpdatedTime = new Array(urls.length).fill(0);
+	let lastProgress = new Array(urls.length).fill(0);
+	
     return new Promise((resolve, reject) => {
-      request(url)
+      request({
+        url,
+        rejectUnauthorized: false
+      })
         .on('response', response => {
           totalBytes = parseInt(response.headers['content-length'], 10);
         })
         .on('data', chunk => {
           receivedBytes += chunk.length;
-          const progress = (receivedBytes / totalBytes) * 100;
-		  
-		  // Send a progress update to all connected WebSocket clients
-          wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ index, progress }));
+          const progress = ((receivedBytes / totalBytes) * 100).toFixed(1);;
+          if (progress - lastProgress[index] > 0.1) { // update progress bar if there is a progress
+            //console.log('indedx : ' + index + ' Progress : ' + progress + ' receivedBytes : ' + receivedBytes + ' totalBytes : ' + totalBytes)
+            if (Date.now() - lastUpdatedTime[index] >= 500) { // update progress bar every 500ms
+              // Send a progress update to all connected WebSocket clients
+              wss.clients.forEach(client => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ index, progress }));
+              }
+            });
+            lastUpdatedTime[index] = Date.now();
+            lastProgress[index] = progress;
             }
-          });
+          }
         })
         .pipe(fs.createWriteStream(downloadLocation))
         .on('close', () => {
+		  wss.clients.forEach(client => {
+			if (client.readyState === WebSocket.OPEN) {
+			  client.send(JSON.stringify({ index, progress: 100 }));
+			}
+		  });
           resolve(downloadLocation);
+        }).on('error', error => {
+		  console.log(error);
+          reject(error);
         });
     });
   });
   // Wait for all downloads to complete and send the download locations to the client
   Promise.all(downloads).then(downloadLocations => {
     res.send(downloadLocations);
+  }).catch(error => {
+    console.log(error);
   });
 });
+
+// Handle Torrent download if you want to use it consider to update to newer version
+function downloadTorrents(magnetURI, wss) {
+  return new Promise((resolve, reject) => {
+    const torrent = client.add(magnetURI, { path: downloadDirectory }, torrent => {
+      const file = torrent.files[0];
+      const downloadLocation = path.join(downloadDirectory, file.path);
+
+      let receivedBytes = 0;
+      let totalBytes = file.length;
+      let lastProgress = -1;
+
+      const intervalId = setInterval(() => {
+        const progress = Math.floor((receivedBytes / totalBytes) * 100);
+        if (progress !== lastProgress) {
+          // Send a progress update to all connected WebSocket clients
+          wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(JSON.stringify({ progress }));
+            }
+          });
+          lastProgress = progress;
+        }
+      }, 1000);
+
+      // Create Read Stream
+      file.createReadStream()
+        .on('data', chunk => {
+          receivedBytes += chunk.length;
+        })
+        .pipe(fs.createWriteStream(downloadLocation))
+        .on('close', () => {
+          clearInterval(intervalId);
+          resolve(downloadLocation);
+        }).on('error', error => {
+          clearInterval(intervalId);
+          reject(error);
+        });
+    });
+  });
+}
 
 // Handle WebSocket connections
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message: ${message}`);
   });
+});
+
+// Define a route to delete file from a given name
+app.get('/delete-file', (req, res) => {
+  const fileName = req.query.fileName;
+  if (!fileName) {
+    res.status(400).send('Missing fileName parameter');
+    return;
+  }
+
+  // Get the file path
+  const filePath = path.join(downloadDirectory, fileName);
+
+  // Delete the file
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error deleting file');
+      return;
+    }
+    res.send('File deleted successfully');
+  });
+});
+
+// Define a route to get all file names in a given path
+app.get("/files", (req, res) => {
+  // Get the path from the query parameter
+  let folder_path = downloadDirectory;
+
+  // Check if the path is valid
+  if (folder_path) {
+    // Resolve the absolute path
+    folder_path = path.resolve(folder_path);
+
+    // Read the file names in the folder
+    fs.readdir(folder_path, (err, files) => {
+      if (err) {
+        // Handle error
+        res.status(500).send("Server error");
+      } else {
+        // Send file names as JSON data
+        res.json(files);
+      }
+    });
+  } else {
+    // Handle missing path
+    res.status(400).send("Missing path parameter");
+  }
 });
 
 // Route to get remaining free memory in MB
@@ -97,20 +232,6 @@ app.get('/remaining-mem', (req, res) => {
 
 // Route to get remaining free disk space in MB
 app.get('/remaining-space', (req, res) => {
-  const drive = path.parse(process.cwd()).root;
-  disk.check(drive, (err, info) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send({ error: 'Error getting disk usage' });
-    } else {
-      const freeSpace = info.free / (1024 * 1024);
-      const roundedFreeSpace = Math.round(freeSpace);
-      res.send({ roundedFreeSpace });
-    }
-  });
-});
-
-/*app.get('/remaining-space', (req, res) => {
 	exec('df -h /', (error, stdout, stderr) => {
     if (error) {
       console.error(error);
@@ -123,4 +244,4 @@ app.get('/remaining-space', (req, res) => {
       res.send({ freeSpace, persent });
     }
   });
-});*/
+});
